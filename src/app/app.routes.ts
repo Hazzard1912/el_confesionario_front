e69eb7d2768063fc3ca3086ba5a2ae8788748268
Hazzard1912 <jhonnier.hernandez@correounivalle.edu.co>
2023-07-14 00:30:18 -0500

    Primer commit, landing page basica creada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingPageComponent,
+  },
+  {
+    path: 'home',
+    component: HomePageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary1: "#2A2D34",
+        primary2: "#DE3C4B",
+        primary3: "#EEF0F2",
+        primary4: "#3F7CAC",
+        primary5: "#8390FA",
+      },
+    },
+  },
+  plugins: [],
+};
